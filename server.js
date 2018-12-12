f4e7c7ad156ed3672fa961ade86c8906c9759e0b
Alexander Rowland <arowland900@gmail.com>
2018-12-12 12:23:54 -0800

    server.js updates

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ mongoose.connect(process.env.MONGODB_URI, (err) => {
     console.log(err || "Connected to MongoDB")
 })
 
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.static('public'));
+
 app.listen(PORT, (err) => {
     console.log(err || `Server running on port ${PORT}`)
 })
