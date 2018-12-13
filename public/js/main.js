6afc75231910e30bbd45c80b400f94374ecea37d
Alexander Rowland <arowland900@gmail.com>
2018-12-13 09:11:06 -0800

    complete

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,73 @@
-console.log("js is loaded")
+console.log("js loaded")
+const apiClient = axios.create()
+const $list = $('#list')
+const $toggle = $('.toggle')
+
+const $addItemBtn = $('#form button')
+const $bodyField = $('#body')
+const $completedField = $('#completed')
+
+apiClient({ method: 'get', url: '/api/todos' }).then((apiResponse) => {
+    // after receiving data, loop through it, and append an <li>
+    const todos = apiResponse.data
+    // console.log(todos)
+    console.log($list)
+    todos.forEach((t) => {
+        $list.append(`
+            <li id="${t._id}">
+                <div class="row">
+                        <span class="lead col-3">${t.body}</span>
+                    
+                        <button type="button" class="btn btn-light completed col-3">${t.completed ? "completed" : "incomplete"}</button>
+                   
+                        <button class="btn btn-outline-dark delete col-3">X</button>
+                </div>  
+                
+            </li> 
+        `) 
+    })
+    
+})
+
+$addItemBtn.on("click", function() {
+    const formData = {
+        body: $bodyField.val(),
+        completed: false
+    }
+    apiClient({ method: 'post', url: '/api/todos', data: formData}).then((apiResponse) => {
+        let item = apiResponse.data;
+        // console.log(item)
+            $list.append(`
+                <li id="${item._id}">
+                    <div class="row">
+                    <span class="lead col-3">${item.body}</span>
+                        
+                    <button type="button" class="btn btn-light completed col-3">${item.completed ? "completed" : "incomplete"}</button>
+            
+                    <button class="btn btn-outline-dark delete col-3">X</button>
+                    </div>
+                </li> 
+            `) 
+    })
+})
+
+$list.on('click', '.delete', function() {
+    const todoId = $(this).parent().parent().attr('id')
+    console.log($(this).parent().parent().attr('id'))
+    // console.log(todoId)
+    apiClient({ method: 'delete', url: `/api/todos/${todoId}`}).then((apiResponse) => {
+        console.log(apiResponse)
+        $('#' + todoId).fadeOut(function() {
+            $(this).remove()
+        })
+    })
+})
+
+$list.on('click', '.completed', function() {
+    const todoId = $(this).parent().parent().attr('id')
+    console.log(todoId)
+    apiClient({ method: 'patch', url: `/api/todos/${todoId}`}).then((apiResponse) => {
+        console.log(apiResponse.data)
+        $(this).text(apiResponse.data.todo.completed ? "Complete" : "Incomplete")
+    })
+})
--- server.js
+++ server.js
@@ -1,21 +1,62 @@
-require('dotenv').config()
+require('dotenv').config() 
 
 const
-    express = require('express'),
-    app = express(),
-    mongoose = require('mongoose'),
-    logger = require('morgan'),
-    Todo = require('./models/Todo'),
-    PORT = 3000
+  express = require('express'),
+  app = express(),
+  mongoose = require('mongoose'),
+  logger = require('morgan'),
+  Todo = require('./models/Todo'),
+  PORT = 3000
 
 mongoose.connect(process.env.MONGODB_URI, (err) => {
-    console.log(err || "Connected to MongoDB")
+  console.log(err || "Connected to MongoDB.")
 })
 
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.static('public'));
 
+
+app.get("/api/todos", (req, res) => {
+  Todo.find({}, (err, todosFromDB) => {
+    if(err) return console.log(err)
+    res.json(todosFromDB)
+  })
+})
+
+app.get("/api/todos/:id", (req, res) => {
+  Todo.findById({ _id: req.params.id }, (err, todoFromDB) => { 
+    if(err) return console.log(err)
+    res.json(todoFromDB); 
+  });
+})
+
+app.post("/api/todos", (req, res) => {
+  Todo.create(req.body, (err, newTodoItem) => {
+    if(err) return console.log(err)
+    res.json(newTodoItem)
+  })
+})
+
+app.delete("/api/todos/:id", (req, res) => {
+  Todo.deleteOne({ _id: req.params.id }, (err) => {
+    if (err) return console.log(err);
+    res.json({ message: "todo item deleted" })
+  });
+})
+
+app.patch("/api/todos/:id", (req, res) => {
+  Todo.findById({_id: req.params.id}, (err, todoItem) => { 
+      if(err) return console.log(err)
+      todoItem.completed = !todoItem.completed
+      todoItem.save((err, updatedTodo) => {
+        if(err) return console.log(err)
+        res.json({ message: "item updated!", todo: updatedTodo })
+      })
+  })
+})
+
+
 app.listen(PORT, (err) => {
-    console.log(err || `Server running on port ${PORT}`)
+  console.log(err || `Server running on port ${PORT}.`)
 })
